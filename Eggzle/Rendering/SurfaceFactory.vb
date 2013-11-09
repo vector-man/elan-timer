5d33ea90433acb632446935b2432374e69ba9fab
Michael <admin@codeisle.com>
2013-11-09 06:48:06 -0500

    Added render rate to constructor.

--- Eggzle/Rendering/SurfaceFactory.vb
+++ Eggzle/Rendering/SurfaceFactory.vb
@@ -1,13 +1,13 @@
 ﻿Namespace Rendering
     Public Class SurfaceFactory
-        Public Shared Function CreateInstance(renderer As Rendering.IRenderer, args As RenderArgs)
-            Return CreateInstance(renderer, args, False)
+        Public Shared Function CreateInstance(renderer As Rendering.IRenderer, args As RenderArgs, renderRate As Integer)
+            Return CreateInstance(renderer, args, False, renderRate)
         End Function
-        Public Shared Function CreateInstance(renderer As IRenderer, args As RenderArgs, preview As Boolean)
+        Public Shared Function CreateInstance(renderer As IRenderer, args As RenderArgs, preview As Boolean, renderRate As Integer)
             If preview Then
                 Return New PreviewSurface(renderer, args, 1)
             Else
-                Return New Surface(renderer, args)
+                Return New Surface(renderer, args, renderRate)
             End If
         End Function
     End Class
