04267e4d12aa709553fc9a45eb6ea92bcaef470b
Michael <admin@codeisle.com>
2013-11-12 15:57:35 -0500

    Added new classes for rendering.

---
+++ Eggzle/Rendering/RenderObjects/TextRenderObject.vb
@@ -0,0 +1,64 @@
+﻿Public Class TextRenderObject
+    Implements IRenderObject
+    Const MaximumFontSize = 1000
+    Sub New(text As String, font As Font, sizeToFit As Boolean, color As Color, stringFormat As StringFormat, visible As Boolean)
+        MyClass.Text = text
+        MyClass.Font = font
+        MyClass.Color = color
+        MyClass.SizeToFit = sizeToFit
+        MyClass.Visible = visible
+        MyClass.StringFormat = stringFormat
+    End Sub
+
+    Public Property Visible As Boolean Implements IRenderObject.Visible
+    Public Property SizeToFit As Boolean
+    Public Property Text As String
+    Public Property Font As Font
+    Public Property Color As Color
+
+    Public Property StringFormat As StringFormat
+    Public Overridable Sub Draw(args As IRenderArgs) Implements IRenderObject.Draw
+        If (Visible) Then
+            Dim prefferedFontSize As Long
+            If (MyClass.SizeToFit) Then
+                prefferedFontSize = MaximumFontSize
+            Else
+                prefferedFontSize = MyClass.Font.Size
+            End If
+            Using textFont = AppropriateFont(args.Graphics, MyClass.Font.Size, prefferedFontSize, args.Rectangle.Size, MyClass.Text, MyClass.Font)
+                Using textBrush As New SolidBrush(MyClass.Color)
+                    args.Graphics.DrawString(MyClass.Text, textFont, textBrush, args.Rectangle, MyClass.StringFormat)
+                End Using
+            End Using
+        End If
+    End Sub
+
+    Private Function AppropriateFont(ByVal g As System.Drawing.Graphics, ByVal minFontSize As Integer, ByVal maxFontSize As Integer, ByVal layoutSize As Size, ByVal s As String, ByVal f As Font) As Font
+        If maxFontSize = minFontSize Then
+            f = New Font(f.FontFamily, minFontSize, f.Style)
+        End If
+        Dim extent As SizeF
+        extent = g.MeasureString(s, f)
+
+        If maxFontSize <= minFontSize Then
+            Return f
+        End If
+
+        Dim hRatio As Single = layoutSize.Height / extent.Height
+        Dim wRatio As Single = layoutSize.Width / extent.Width
+
+        Dim ratio As Single = If((hRatio < wRatio), hRatio, wRatio)
+
+        Dim newSize As Single = f.Size * ratio
+
+        If newSize < minFontSize Then
+            newSize = minFontSize
+        ElseIf newSize > maxFontSize Then
+            newSize = maxFontSize
+        End If
+
+        f = New Font(f.FontFamily, newSize, f.Style)
+
+        Return f
+    End Function
+End Class
---
+++ Eggzle/Rendering/RenderObjects/TimerTextRenderObject.vb
@@ -0,0 +1,19 @@
+﻿Imports Eggzle.CodeIsle.Timers
+
+Public Class TimerTextRenderObject
+    Inherits TextRenderObject
+
+    Sub New(timer As TimerBase, font As Font, format As String, formatProvider As IFormatProvider, sizeToFit As Boolean, color As Color, stringFormat As StringFormat, visible As Boolean)
+        MyBase.New(String.Empty, font, sizeToFit, color, stringFormat, visible)
+        MyClass.Format = format
+        MyClass.FormatProvider = formatProvider
+        MyClass.Timer = timer
+    End Sub
+    Public Property Format As String
+    Public Property FormatProvider As IFormatProvider
+    Public Property Timer As TimerBase
+    Overrides Sub Draw(args As IRenderArgs)
+        MyBase.Text = String.Format(MyClass.FormatProvider, String.Concat("{0:", MyClass.Format, "}"), MyClass.Timer.Current)
+        MyBase.Draw(args)
+    End Sub
+End Class
