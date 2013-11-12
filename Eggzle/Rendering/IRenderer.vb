b05fd1de7afbc86d5fcaba09df8865677a13fc01
Michael <admin@codeisle.com>
2013-11-12 15:56:25 -0500

    Rewrote and moved interface.

--- Eggzle/Rendering/IRenderer.vb
+++ Eggzle/Rendering/IRenderer.vb
@@ -3,6 +3,6 @@ Imports System.Drawing
 Imports System.Windows.Forms
 Namespace Rendering
     Public Interface IRenderer
-        Sub Render(args As RenderArgs)
+        Sub Render(args As IRenderArgs)
     End Interface
 End Namespace
