ecdd124767c3677ff3bff83229cce8970b02177f
Michael <admin@codeisle.com>
2013-11-12 15:58:28 -0500

    Rewrote and moved class.

---
+++ Eggzle/Rendering/RenderObjects/RenderArgs.vb
@@ -0,0 +1,7 @@
+﻿Public Class RenderArgs
+    Implements IRenderArgs
+
+    Public Property Graphics As Graphics Implements IRenderArgs.Graphics
+
+    Public Property Rectangle As Rectangle Implements IRenderArgs.Rectangle
+End Class
