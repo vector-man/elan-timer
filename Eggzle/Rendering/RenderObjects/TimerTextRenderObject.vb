d050e2446285591d8e72ebc92f4db2af36bcfe60
Michael <admin@codeisle.com>
2013-11-16 19:45:14 -0500

    Used 'Me' instead of 'MyClass.'

--- Eggzle/Rendering/RenderObjects/TimerTextRenderObject.vb
+++ Eggzle/Rendering/RenderObjects/TimerTextRenderObject.vb
@@ -5,15 +5,15 @@ Public Class TimerTextRenderObject
 
     Sub New(timer As TimerBase, font As Font, format As String, formatProvider As IFormatProvider, sizeToFit As Boolean, color As Color, stringFormat As StringFormat, visible As Boolean)
         MyBase.New(String.Empty, font, sizeToFit, color, stringFormat, visible)
-        MyClass.Format = format
-        MyClass.FormatProvider = formatProvider
-        MyClass.Timer = timer
+        Me.Format = format
+        Me.FormatProvider = formatProvider
+        Me.Timer = timer
     End Sub
     Public Property Format As String
     Public Property FormatProvider As IFormatProvider
     Public Property Timer As TimerBase
     Overrides Sub Draw(args As IRenderArgs)
-        MyBase.Text = String.Format(MyClass.FormatProvider, String.Concat("{0:", MyClass.Format, "}"), MyClass.Timer.Current)
+        MyBase.Text = String.Format(FormatProvider, String.Concat("{0:", Me.Format, "}"), Me.Timer.Current)
         MyBase.Draw(args)
     End Sub
 End Class
