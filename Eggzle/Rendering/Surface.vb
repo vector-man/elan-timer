f67dc8820fc2569d2b78d780342e5a9505b80903
Michael <admin@codeisle.com>
2013-11-19 15:23:56 -0500

    Overloaded'Enabled' property.

--- Eggzle/Rendering/Surface.vb
+++ Eggzle/Rendering/Surface.vb
@@ -5,6 +5,7 @@
         Private framerate As Integer
         Private surfaceInvalidatorCancellationTokenSource As System.Threading.CancellationTokenSource
         Private renderArgs As RenderArgs
+        Private _enabled As Boolean = True
         Sub New(renderer As IRenderer, framerate As Integer)
             MyBase.New()
             Me.SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
@@ -13,8 +14,7 @@
             MyClass.framerate = framerate
             renderArgs = New RenderArgs
             AddHandler Me.Paint, AddressOf Surface_Paint
-            surfaceInvalidatorCancellationTokenSource = New System.Threading.CancellationTokenSource
-            TaskEx.Run(Sub() SurfaceInvalidatorAsync(surfaceInvalidatorCancellationTokenSource.Token), surfaceInvalidatorCancellationTokenSource.Token)
+            Me.Enabled = True
         End Sub
 
         Private Sub Surface_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs)
@@ -35,5 +35,20 @@
             Task.WaitAll()
             MyBase.Dispose()
         End Sub
+        Public Overloads Property Enabled
+            Get
+                Return _enabled
+            End Get
+            Set(value)
+                _enabled = value
+                If _enabled = False Then
+                    surfaceInvalidatorCancellationTokenSource.Cancel()
+                Else
+                    surfaceInvalidatorCancellationTokenSource = New System.Threading.CancellationTokenSource
+                    TaskEx.Run(Sub() SurfaceInvalidatorAsync(surfaceInvalidatorCancellationTokenSource.Token), surfaceInvalidatorCancellationTokenSource.Token)
+                End If
+            End Set
+        End Property
+
     End Class
 End Namespace
