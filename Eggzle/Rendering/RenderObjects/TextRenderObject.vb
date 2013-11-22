ad89e52b696f321a42adeb5146a0558723af52e0
Michael <admin@codeisle.com>
2013-11-21 21:26:09 -0500

    Added colorBrush and removed the using blocks for creating brushes.

--- Eggzle/Rendering/RenderObjects/TextRenderObject.vb
+++ Eggzle/Rendering/RenderObjects/TextRenderObject.vb
@@ -1,10 +1,11 @@
 ﻿Public Class TextRenderObject
-    Implements IRenderObject
+    Implements IRenderObject, IDisposable
     Const MaximumFontSize = 1000
+    Private colorBrush As SolidBrush
     Sub New(text As String, font As Font, sizeToFit As Boolean, color As Color, stringFormat As StringFormat, visible As Boolean)
         MyClass.Text = text
         MyClass.Font = font
-        MyClass.Color = color
+        colorBrush = New SolidBrush(color)
         MyClass.SizeToFit = sizeToFit
         MyClass.Visible = visible
         MyClass.StringFormat = stringFormat
@@ -15,6 +16,13 @@
     Public Overridable Property Text As String
     Public Property Font As Font
     Public Property Color As Color
+        Get
+            Return colorBrush.Color
+        End Get
+        Set(value As Color)
+            colorBrush.Color = value
+        End Set
+    End Property
     Public Property StringFormat As StringFormat
 
     Public Overridable Sub Draw(args As IRenderArgs) Implements IRenderObject.Draw
@@ -28,9 +36,7 @@
             End If
 
             Using textFont = AppropriateFont(args.Graphics, Font.Size, largestFontSize, args.Rectangle.Size, Text, Font)
-                Using textBrush As New SolidBrush(Color)
-                    args.Graphics.DrawString(Text, textFont, textBrush, args.Rectangle, StringFormat)
-                End Using
+                args.Graphics.DrawString(Text, textFont, colorBrush, args.Rectangle, StringFormat)
             End Using
         End If
     End Sub
@@ -63,4 +69,36 @@
 
         Return f
     End Function
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                colorBrush.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        Me.disposedValue = True
+    End Sub
+
+    ' TODO: override Finalize() only if Dispose(ByVal disposing As Boolean) above has code to free unmanaged resources.
+    'Protected Overrides Sub Finalize()
+    '    ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
+    '    Dispose(False)
+    '    MyBase.Finalize()
+    'End Sub
+
+    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
 End Class
