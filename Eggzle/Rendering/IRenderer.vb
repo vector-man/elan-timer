0cdb8b540c1e471ede947a205c3c604a85960333
Michael <admin@codeisle.com>
2013-11-22 00:04:57 -0500

    Removed old, unused files.

--- Eggzle/Rendering/IRenderer.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System
-Imports System.Drawing
-Imports System.Windows.Forms
-Namespace Rendering
-    Public Interface IRenderer
-        Sub Render(args As IRenderArgs)
-    End Interface
-End Namespace
