66f1b5226ffdeca28d14824ba1e32ec32512b8b9
Michael <admin@codeisle.com>
2013-11-23 17:05:41 -0500

    backgroundObject is now a colored panel.

--- Eon Egg Timer/Forms/FormMain.vb
+++ Eon Egg Timer/Forms/FormMain.vb
@@ -8,7 +8,7 @@ Public Class FormMain
 
     Private ReadOnly EscapeKeyChar = Convert.ToChar(27)
 
-    Private backgroundObject As BackgroundRenderObject
+    Private backgroundObject As Panel
     Private timerObject As TimerTextRenderObject
     Private noteObject As TextRenderObject
     Private stringFormat As New StringFormat
@@ -260,7 +260,8 @@ Public Class FormMain
     Private Async Sub StartUpRendering(timer As EonEggTimer.CodeIsle.Timers.AlarmTimer)
         updateCancellationTokenSource = New System.Threading.CancellationTokenSource
 
-        backgroundObject = New BackgroundRenderObject(Common.Look.BackgroundColor, True)
+        backgroundObject = New Panel
+        backgroundObject.BackColor = Common.Look.BackgroundColor
 
         stringFormat = New StringFormat(System.Drawing.StringFormat.GenericTypographic)
         stringFormat.Alignment = StringAlignment.Center
@@ -270,16 +271,19 @@ Public Class FormMain
         noteObject = New TextRenderObject(Common.Time.Note, Common.Look.Font, Common.Look.SizeToFit, Common.Look.ForegroundColor, stringFormat, False)
         Dim objects As New List(Of IRenderObject)
 
-        objects.Add(backgroundObject)
         objects.Add(timerObject)
         objects.Add(noteObject)
         renderer = New Renderer(objects)
         timerSurface = Rendering.SurfaceFactory.CreateInstance(renderer, Common.Framerate)
-
+        timerSurface.BackColor = Color.Transparent
         AddHandler timerSurface.DoubleClick, AddressOf TimerSurface_DoubleClick
         AddHandler timerSurface.Click, AddressOf TimerSurface_Click
+
+        backgroundObject.Controls.Add(timerSurface)
         timerSurface.Dock = DockStyle.Fill
-        PanelTimer.Controls.Add(timerSurface)
+
+        PanelTimer.Controls.Add(backgroundObject)
+        backgroundObject.Dock = DockStyle.Fill
 
         AddHandler timerSurface.Paint, AddressOf FormMainProgressUpdate
     End Sub
@@ -407,7 +411,7 @@ Public Class FormMain
             Dim noteVisible = noteObject.Visible
             noteObject.Visible = False
 
-            backgroundObject.Color = Common.Look.BackgroundColor
+            backgroundObject.BackColor = Common.Look.BackgroundColor
             timerObject.Color = Common.Look.ForegroundColor
             timerObject.Font = Common.Look.Font
             timerObject.Format = Common.Look.DisplayFormat
