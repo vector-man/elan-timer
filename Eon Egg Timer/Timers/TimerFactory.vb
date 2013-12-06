63f4b53a8104dbdfe78a4d2a3e46951aca9566f2
Michael <admin@codeisle.com>
2013-12-06 07:38:00 -0500

    Added 1 for expiration poll rate parameter.

--- Eon Egg Timer/Timers/TimerFactory.vb
+++ Eon Egg Timer/Timers/TimerFactory.vb
@@ -1,9 +1,9 @@
 ﻿Public Class TimerFactory
     Public Shared Function CreateInstance(duration As TimeSpan, countUp As Boolean, restarts As Integer, alarm As Alarm, alarmEnabled As Boolean)
         If countUp Then
-            Return New CodeIsle.Timers.CountUpAlarmTimer(duration, alarm, alarmEnabled, restarts)
+            Return New CodeIsle.Timers.CountUpAlarmTimer(duration, alarm, alarmEnabled, restarts, 1)
         Else
-            Return New CodeIsle.Timers.CountDownAlarmTimer(duration, alarm, alarmEnabled, restarts)
+            Return New CodeIsle.Timers.CountDownAlarmTimer(duration, alarm, alarmEnabled, restarts, 1)
         End If
     End Function
 End Class
