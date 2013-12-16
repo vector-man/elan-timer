a4e1774cc77ab06ba96a66250eb6a18fa997842e
Michael <admin@codeisle.com>
2013-12-16 17:01:32 -0500

    Switched stopwatch code to official .NET decompiled code.

--- Elan Timer/Timers/Stopwatch.vb
+++ Elan Timer/Timers/Stopwatch.vb
@@ -1,69 +1,111 @@
 ﻿Namespace CodeIsle.Timers
     Public Class Stopwatch
+        Private m_elapsed As Long
         Private startTimeStamp As Long
-        Private _elapsed As Long
-        Private _isRunning As Boolean
-        Private _isReset As Boolean
-        Sub New()
-            Reset()
-        End Sub
-        Public ReadOnly Property Elapsed As TimeSpan
+        Private m_isRunning As Boolean
+        Public Shared ReadOnly Frequency As Long
+        Public Shared ReadOnly IsHighResolution As Boolean
+        Private Shared ReadOnly tickFrequency As Double
+        Private Const TicksPerMillisecond As Long = 10000L
+        Private Const TicksPerSecond As Long = 10000000L
+
+        Public ReadOnly Property IsRunning() As Boolean
+            Get
+                Return Me.m_isRunning
+            End Get
+        End Property
+
+
+        Public ReadOnly Property Elapsed() As TimeSpan
             Get
                 Return New TimeSpan(Me.GetElapsedDateTimeTicks())
             End Get
         End Property
 
-        Public ReadOnly Property ElapsedMilliseconds As Long
+
+        Public ReadOnly Property ElapsedMilliseconds() As Long
             Get
-                Return Me.GetElapsedDateTimeTicks() / 10000L
+                Return Me.GetElapsedDateTimeTicks() \ 10000L
             End Get
         End Property
 
-        Public ReadOnly Property ElapsedTicks As Long
+
+        Public ReadOnly Property ElapsedTicks() As Long
             Get
-                Return GetRawElapsedTicks()
-            End Get
-        End Property
-        Public ReadOnly Property IsRunning As Boolean
-            Get
-                Return _isRunning
+                Return Me.GetRawElapsedTicks()
             End Get
         End Property
 
-        Public Function GetTimeStamp() As Long
+        Shared Sub New()
+            Stopwatch.IsHighResolution = False
+            Stopwatch.Frequency = 10000000L
+            Stopwatch.tickFrequency = 1.0
+        End Sub
+
+
+        Public Sub New()
+            Me.Reset()
+        End Sub
+
+
+        Public Sub Start()
+            If Me.m_isRunning Then
+                Return
+            End If
+            Me.startTimeStamp = Stopwatch.GetTimestamp()
+            Me.m_isRunning = True
+        End Sub
+
+
+        Public Shared Function StartNew() As Stopwatch
+            Dim stopwatch As New Stopwatch()
+            stopwatch.Start()
+            Return stopwatch
+        End Function
+
+        Public Sub [Stop]()
+            If Not Me.m_isRunning Then
+                Return
+            End If
+            Me.m_elapsed += Stopwatch.GetTimestamp() - Me.startTimeStamp
+            Me.m_isRunning = False
+            If Me.m_elapsed >= 0L Then
+                Return
+            End If
+            Me.m_elapsed = 0L
+        End Sub
+
+
+        Public Sub Reset()
+            Me.m_elapsed = 0L
+            Me.m_isRunning = False
+            Me.startTimeStamp = 0L
+        End Sub
+
+
+        Public Sub Restart()
+            Me.m_elapsed = 0L
+            Me.startTimeStamp = Stopwatch.GetTimestamp()
+            Me.m_isRunning = True
+        End Sub
+
+
+        Public Shared Function GetTimestamp() As Long
             Return DateTime.UtcNow.Ticks
         End Function
-        Public Sub Reset()
-            _isRunning = False
-            _isReset = True
-            _elapsed = New TimeSpan(0)
-        End Sub
-        Public Sub Restart()
-            [Stop]()
-            Reset()
-            Start()
-        End Sub
-        Public Sub Start()
-            _isRunning = True
-            _initial = DateTime.UtcNow
-        End Sub
-        Public Sub StartNew()
-            Restart()
-        End Sub
-        Public Sub [Stop]()
-            _isRunning = False
-            _isReset = False
-        End Sub
+
         Private Function GetRawElapsedTicks() As Long
-            Dim num1 As Long = _elapsed
-            If Me.IsRunning Then
-                Dim num2 As Long = Me.GetTimeStamp() - Me.startTimeStamp
+            Dim num1 As Long = Me.m_elapsed
+            If Me.m_isRunning Then
+                Dim num2 As Long = Stopwatch.GetTimestamp() - Me.startTimeStamp
                 num1 += num2
             End If
             Return num1
         End Function
-        Private Function GetElapsedDateTimeTicks() As Long
 
+        Private Function GetElapsedDateTimeTicks() As Long
+            Dim rawElapsedTicks As Long = Me.GetRawElapsedTicks()
+            Return rawElapsedTicks
         End Function
     End Class
 End Namespace
