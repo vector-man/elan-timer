59e1aae3ec94939aad49f294868990e7be3fbfb9
Michael <admin@codeisle.com>
2014-11-18 14:41:18 -0500

    Time format provider handles other formats.

--- Elan Timer/Timers/TimeFormat.vb
+++ Elan Timer/Timers/TimeFormat.vb
@@ -18,38 +18,51 @@ Public Class TimeFormat : Implements IFormatProvider, ICustomFormatter
         countUpFormat = res.GetResourceValue("CountUpFormat")
     End Sub
     Public Function Format(fmt As String, arg As Object, formatProvider As IFormatProvider) As String Implements ICustomFormatter.Format
-        Dim sb = New StringBuilder()
-        Dim ts = CType(arg, TimeSpan)
-        Select Case fmt
-            Case "s", Nothing
-                ' Show seconds - format: hours:mm:ss
-                sb.Append(Math.Floor(ts.TotalHours))
-                sb.Append(ts.ToString("\:mm\:ss"))
-            Case "S"
-                ' Show only seconds - format: s
-                sb.Append(Math.Floor(ts.TotalSeconds))
-            Case "m"
-                ' Show microseconds - format: hours:mm:ss.ff
-                sb.Append(Math.Floor(ts.TotalHours))
-                sb.Append(ts.ToString("\:mm\:ss\.ff"))
-            Case "d"
-                ' Show deciseconds - format: hours:mm:ss.f
-                sb.Append(Math.Floor(ts.TotalHours))
-                sb.Append(ts.ToString("\:mm\:ss\.f"))
-            Case "v"
-                Dim totalHours As Integer = Math.Floor(ts.TotalHours)
-                ' Show text (verbal) - format: d days format h hours m minutes s seconds.
-                If (totalHours > 0 And (Not ts.Minutes > 0)) Then
-                    Return String.Format(hoursSecondsFormat, totalHours, ts.Seconds)
-                ElseIf (totalHours > 0) Then
-                    Return String.Format(hoursFormat, totalHours, ts.Minutes, ts.Seconds)
-                ElseIf (ts.Minutes > 0) Then
-                    Return String.Format(minutesFormat, ts.Minutes, ts.Seconds)
-                Else
-                    Return String.Format(secondsFormat, ts.Seconds)
-                End If
-        End Select
-        Return sb.ToString.Trim
+        If (TypeOf arg Is TimeSpan) Then
+            Dim sb = New StringBuilder()
+            Dim ts = CType(arg, TimeSpan)
+            Select Case fmt
+                Case "s", Nothing
+                    ' Show seconds - format: hours:mm:ss
+                    sb.Append(Math.Floor(ts.TotalHours))
+                    sb.Append(ts.ToString("\:mm\:ss"))
+                Case "S"
+                    ' Show only seconds - format: s
+                    sb.Append(Math.Floor(ts.TotalSeconds))
+                Case "m"
+                    ' Show microseconds - format: hours:mm:ss.ff
+                    sb.Append(Math.Floor(ts.TotalHours))
+                    sb.Append(ts.ToString("\:mm\:ss\.ff"))
+                Case "d"
+                    ' Show deciseconds - format: hours:mm:ss.f
+                    sb.Append(Math.Floor(ts.TotalHours))
+                    sb.Append(ts.ToString("\:mm\:ss\.f"))
+                Case "v"
+                    Dim totalHours As Integer = Math.Floor(ts.TotalHours)
+                    ' Show text (verbal) - format: d days format h hours m minutes s seconds.
+                    If (totalHours > 0 And (Not ts.Minutes > 0)) Then
+                        Return String.Format(hoursSecondsFormat, totalHours, ts.Seconds)
+                    ElseIf (totalHours > 0) Then
+                        Return String.Format(hoursFormat, totalHours, ts.Minutes, ts.Seconds)
+                    ElseIf (ts.Minutes > 0) Then
+                        Return String.Format(minutesFormat, ts.Minutes, ts.Seconds)
+                    Else
+                        Return String.Format(secondsFormat, ts.Seconds)
+                    End If
+            End Select
+            Return sb.ToString.Trim
+        End If
+        Return HandleOtherFormats(fmt, arg)
+    End Function
+
+    Private Function HandleOtherFormats(fmt As String, arg As Object) As String
+        If TypeOf arg Is IFormattable Then
+            Return DirectCast(arg, IFormattable).ToString(fmt, CultureInfo.CurrentCulture)
+        ElseIf arg IsNot Nothing Then
+            Return arg.ToString()
+        Else
+            Return String.Empty
+        End If
     End Function
 
     Public Function GetFormat(formatType As Type) As Object Implements IFormatProvider.GetFormat
