b6a703447354a13fd0c73bc2d27c15303f8656f6
Michael <admin@codeisle.com>
2014-11-18 18:06:06 -0500

    Added unit tests.

---
+++ Elan TimerTests/Audio/SoundTests.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports ElanTimer
+
+Namespace ElanTimer.Tests
+    <TestClass()> Public Class SoundTests
+        Private playbackCount = 0
+        <TestMethod()> Public Sub LoadTest()
+            Dim sound As New Sound()
+            AddHandler sound.PlaybackStopped, AddressOf Sound_PlaybackStopped
+            sound.Load("C:\Users\Michael\Documents\Visual Studio 2013\Projects\Elan Timer\bin\Debug\Data\Alarms\Military Alarm.wav")
+            sound.Play()
+            sound.Stop()
+            sound.Load("C:\Users\Michael\Documents\Visual Studio 2013\Projects\Elan Timer\bin\Debug\Data\Alarms\Military Alarm.wav")
+            sound.Play()
+            sound.Stop()
+
+            Assert.AreEqual(2, playbackCount)
+        End Sub
+
+        Private Sub Sound_PlaybackStopped()
+            playbackCount += 1
+        End Sub
+
+    End Class
+
+
+End Namespace
+
+
---
+++ Elan TimerTests/Timers/TimerBaseTests.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports ElanTimer.CodeIsle.Timers
+
+
+Namespace ElanTimer.CodeIsle.Timers.Tests
+
+    <TestClass()> Public Class TimerBaseTests
+
+        <TestMethod()> Public Sub ResetTest()
+            Dim timer As New AlarmTimer(TimeSpan.Zero)
+
+            timer.Reset(New TimeSpan(0, 0, 0, 10), New TimeSpan(0, 0, 32), 3)
+
+            Assert.AreEqual(3, timer.ElapsedRestarts)
+            Assert.AreEqual(New TimeSpan(0, 0, 2), timer.Elapsed)
+        End Sub
+    End Class
+
+
+End Namespace
+
+
