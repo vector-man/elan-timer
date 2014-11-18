2af9a77116997b255925cb467cb76564549d6f62
Michael <admin@codeisle.com>
2014-11-18 15:01:30 -0500

    Modified to use TaskModel list instead of TasksModel class.

--- Elan Timer/Settings/TaskImportingEventArgs.vb
+++ Elan Timer/Settings/TaskImportingEventArgs.vb
@@ -1,11 +1,11 @@
 ﻿Public Class TaskImportingEventArgs
     Inherits EventArgs
-    Sub New(input As String, tasks As TasksModel)
+    Sub New(input As String, tasks As List(Of TaskModel))
         MyBase.New()
         Me.Tasks = tasks
         Me.Input = input
     End Sub
     Public Property Input As String
-    Public Property Tasks As TasksModel
+    Public Property Tasks As List(Of TaskModel)
 End Class
 
